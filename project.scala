a291233321fe37a80404e98b038343137340b2fe
canxin <q1969730106@gmail.com>
2025-03-27 09:51:38 +0800

    [Ini]

---
+++ project.scala
@@ -0,0 +1,5 @@
+//> using scala "2.13.12"
+//> using dep "org.chipsalliance::chisel:6.7.0"
+//> using plugin "org.chipsalliance:::chisel-plugin:6.7.0"
+//> using dep "edu.berkeley.cs::chiseltest:6.0.0"
+//> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ywarn-unused", "-Ymacro-annotations"
